03e2ec1c3a671b33e786a8b1f149b64a77f351ee
silvicardo <silvicardo@gmail.com>
2018-11-21 13:25:57 +0100

    project files and logic settings

---
+++ main.js
@@ -0,0 +1,28 @@
+
+// CENSURATORE
+//  - Il software riceve in input un lungo testo e una serie di parole
+// da censurare.
+// - Restituisce il testo con xxx al posto delle parole censurate.
+// - Stampa un "badword index" calcolato come il numero di
+// parole censurate su il numero di parole totali
+
+
+var testoLoremDaCensurare = produciTestoIniziale()
+
+var arrayParoleDaCensurare = ['ipsum', 'bacon', 'prosciutto'];
+
+sostituisciConXXXParoleIn(testoLoremDaCensurare, arrayParoleDaCensurare);
+
+// FUNZIONI
+function sostituisciConXXXParoleIn(testo, arrayParoleDaCensurare) {
+
+  var testoFinale;
+
+  //GeneraMeccanismoCensura
+
+  return testoFinale;
+}
+
+function produciTestoIniziale() {
+  return "Bacon ipsum dolor amet landjaeger fatback burgdoggen pork chop, sausage ball tip sirloin boudin pastrami venison pork kevin. Sausage burgdoggen porchetta tail ribeye shoulder, jowl venison. Fatback boudin ham hock ball tip ground round jerky, pork brisket pastrami biltong beef ribs shank turducken salami short ribs. Chicken salami boudin, buffalo ground round pastrami fatback. Doner pancetta pig filet mignon pork loin brisket. Pig meatball jerky boudin andouille. Prosciutto pig shoulder beef sirloin buffalo. Sausage buffalo venison hamburger spare ribs kielbasa, burgdoggen filet mignon brisket. Picanha ham prosciutto shoulder, biltong frankfurter boudin. Short loin pork belly jowl turkey meatloaf pork loin. Landjaeger spare ribs hamburger ham hock tri-tip doner bacon corned beef tongue. Ribeye tongue sirloin, pig pastrami andouille chuck kevin drumstick hamburger landjaeger shank burgdoggen pork. Capicola chuck ribeye, sausage cow sirloin swine flank ground round. Salami short ribs sirloin biltong pork chop chuck andouille drumstick jerky beef ribs tri-tip. Pork loin pancetta ribeye salami sausage, leberkas pork belly kevin hamburger t-bone brisket flank beef ribs pastrami. Ham hock turkey buffalo shank short ribs, pork loin strip steak andouille boudin hamburger. Tri-tip prosciutto bresaola shoulder andouille burgdoggen swine ball tip tongue leberkas.Bacon ipsum dolor amet landjaeger fatback burgdoggen pork chop, sausage ball tip sirloin boudin pastrami venison pork kevin. Sausage burgdoggen porchetta tail ribeye shoulder, jowl venison. Fatback boudin ham hock ball tip ground round jerky, pork brisket pastrami biltong beef ribs shank turducken salami short ribs. Chicken salami boudin, buffalo ground round pastrami fatback. Doner pancetta pig filet mignon pork loin brisket. Pig meatball jerky boudin andouille. Prosciutto pig shoulder beef sirloin buffalo. Sausage buffalo venison hamburger spare ribs kielbasa, burgdoggen filet mignon brisket. Picanha ham prosciutto shoulder, biltong frankfurter boudin. Short loin pork belly jowl turkey meatloaf pork loin. Landjaeger spare ribs hamburger ham hock tri-tip doner bacon corned beef tongue. Ribeye tongue sirloin, pig pastrami andouille chuck kevin drumstick hamburger landjaeger shank burgdoggen pork. Capicola chuck ribeye, sausage cow sirloin swine flank ground round. Salami short ribs sirloin biltong pork chop chuck andouille drumstick jerky beef ribs tri-tip. Pork loin pancetta ribeye salami sausage, leberkas pork belly kevin hamburger t-bone brisket flank beef ribs pastrami. Ham hock turkey buffalo shank short ribs, pork loin strip steak andouille boudin hamburger. Tri-tip prosciutto bresaola shoulder andouille burgdoggen swine ball tip tongue leberkas."
+}
